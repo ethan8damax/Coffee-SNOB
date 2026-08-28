208b8b71813cc49aa9bfa1d27a0d93b0b2bfae6d
ethan8damax <ethan2damax@gmail.com>
2026-08-28 14:05:33 -0400

    Add shared APP_URL constant for the deployed app's sign-in/sign-up links

---
+++ apps/web/lib/app-url.ts
@@ -0,0 +1,5 @@
+export const APP_URL =
+  process.env.NEXT_PUBLIC_APP_URL ?? "https://app.coffeesnobproject.com";
+
+export const APP_SIGN_UP_URL = `${APP_URL}/sign-up`;
+export const APP_SIGN_IN_URL = `${APP_URL}/sign-in`;
